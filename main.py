7b4771106cd7a3edf52eaeede58d9c383c38147c
hollowgoose <tr.hamb@gmail.com>
2024-09-18 11:53:36 +0100

    Player movement added

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
+    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
 
         screen.fill("black")
         player.draw(screen)
